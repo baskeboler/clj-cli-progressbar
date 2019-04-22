dbb33a8ac7ccf23bd67079615159d3f176758dba
Victor Gil <baskeboler@gmail.com>
2019-04-21 21:13:06 -0300

    added simple tests

--- test/clj_cli_progress/core_test.clj
+++ test/clj_cli_progress/core_test.clj
@@ -4,4 +4,15 @@
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1)))
+  (testing "wrapped collection"
+    (let [col (range 10000)
+          w (progress-bar-wrapped-collection col "mytask")]
+      (doseq [a w]
+        (inc a))
+      (is true)))
+  (testing "reducttion"
+    (let [col (repeat 10000 1)
+          w (progress-bar-wrapped-collection col "ones")
+          res (reduce + 0 w)]
+      (is (= res 10000)))))
